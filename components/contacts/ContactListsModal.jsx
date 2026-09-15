ac22cb6657973e4f723c22fc8c51dfb006f9292c
Promotergroup <giuseppebusacca@promotergroup.eu>
2026-09-15 15:57:43 +0200

    fix: close outer modal div tag & release v1.6.3

--- components/contacts/ContactListsModal.jsx
+++ components/contacts/ContactListsModal.jsx
@@ -780,6 +780,7 @@ useEffect(() => {
           </div>
         </div>
       </div>
+    </div>
 
       {confirmRemoveFromList && (
         <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-[60] p-4 animate-in fade-in duration-200">
