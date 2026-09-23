e65802028d0fdbc5ba67e8cb43aea6f792d315a0
Promotergroup <giuseppebusacca@promotergroup.eu>
2026-09-23 10:02:39 +0200

    Fix JSX closing tag in EmailPlatform.jsx profile modal

--- components/email/EmailPlatform.jsx
+++ components/email/EmailPlatform.jsx
@@ -26918,7 +26918,6 @@ if (loadingProfile && !user && !authUser) {
                           Cambia Password
                         </button>
                       </div>
-                    </div>
                       {/* Contenuto basato sulla selezione del tab */}
                       {activeProfileTab === 'notifiche2' && <div>Sezione Notifiche</div>}
                       {activeProfileTab === 'lingua' && <div>Sezione Lingua</div>}                     
@@ -26926,7 +26925,6 @@ if (loadingProfile && !user && !authUser) {
                       {activeProfileTab === 'privacy' && <div>Sezione Privacy</div>}
                       {activeProfileTab === 'session' && <div>Sezione Timeout</div>}
                       {activeProfileTab === 'cambia-password' && <div>Sezione Cambia password</div>}
-                    </div>
                   </nav>
                 </div>
 
